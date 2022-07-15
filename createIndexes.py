b95c9c090903cd1837846bb18d8f20ab47900eb7
Bruno Trevelin <brunonunest@Brunos-Air.Home>
2022-07-15 13:38:49 +0100

    updatecode

--- createIndexes.py
+++ createIndexes.py
@@ -8,13 +8,13 @@ ssl._create_default_https_context = ssl._create_unverified_context
 try:
     mongourl = config('MONGO_URL')
     client = pymongo.MongoClient(mongourl)
-    db = client.prod
+    db = client.prodmainnet
     index1 = db.floorpricebyday.create_index([('datetime', pymongo.DESCENDING)],unique=True)
     index2 = db.nftvolumes.create_index([('asset', pymongo.ASCENDING)],unique=True)
-    index3 = db.senanftrybyday.create_index([('datetime', pymongo.DESCENDING)],unique=True)
+    index3 = db.senanfgittrybyday.create_index([('datetime', pymongo.DESCENDING)],unique=True)
     index4 = db.senanftvolumebyday.create_index([('datetime', pymongo.DESCENDING)],unique=True)
     index5 = db.totaltransactions.create_index([('datetime', pymongo.DESCENDING)],unique=True)
     index6 = db.tradedvolumebyday.create_index([('datetime', pymongo.DESCENDING)],unique=True)
     print("Indexes created/updated")
 except:
-    print("Error trying to create indexes")
+    print("Error trying to create some indexes, check on MongoDB")
